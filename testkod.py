92ee9ded825097974b831ad51172d1ceedcf5144
Tyraaxen <tyra_axen@hotmail.se>
2023-03-28 16:31:22 +0200

    från tyras WSL #sheesh

--- testkod.py
+++ testkod.py
@@ -1,5 +1,5 @@
 import pyne
-print(f"Oj vad bra!? Pyne funkar nu!")
+print(f"hej ja heter tyra oh har fått det att funka")
 
 '''This code will serve as a framework for the Half Monte Carlo methods. 
 PyNE is needed for the code to work, installation guide: https://pyne.io/install/index.html.
