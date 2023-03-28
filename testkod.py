c937af6d88ffb7d6167110da9b4afbbb357409be
OlleDios <olle.dios1@gmail.com>
2023-03-28 17:08:02 +0200

    hej fråm oööers

--- testkod.py
+++ testkod.py
@@ -1,6 +1,6 @@
 import pyne
-print(f"Oj vad bra!? Pyne funkar nu!")
+print(f"olle testar nu")
 
-'''This code will serve as a framework for the Half Monte Carlo methods. 
+'''This code will serve as a framework for the Half Monte Carlo methods.
 PyNE is needed for the code to work, installation guide: https://pyne.io/install/index.html.
 The code is written for godiva, but can be changed to different problems.'''
