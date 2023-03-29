360664f1295f6b2a4c70b8a3d23c392d1d482f91
AXELWOHLIN <axelwohlin@gmail.com>
2023-03-29 16:15:43 +0200

    mike

--- ace_test.py
+++ ace_test.py
@@ -5,10 +5,29 @@ import requests
 import np_csvimport
 import numpy as np
 
+"""
 url = "https://www-nds.iaea.org/wolfram/w180/beta3/W180.ace"
 r = requests.get(url)
 with open("W180.ace", "wb") as outfile:
     outfile.write(r.content)
+
+lib = pyne.ace.Library('W180.ace')
+lib.read('74180.21c')
+lib.tables
+w180 = lib.tables['74180.21c']
+total_xs = w180.sigma_t
+energy = w180.energy
+"""
+r = 'U235.nuss.10.10.2016/U235-n.ace_0000'
+with open("U235.ace", "wb") as outfile:
+    outfile.write(r.content)
+
+lib = pyne.ace.Library('W180.ace')
+lib.read('74180.21c')
+lib.tables
+w180 = lib.tables['74180.21c']
+total_xs = w180.sigma_t
+energy = w180.energy
 
 lib = pyne.ace.Library('W180.ace')
 lib.read('74180.21c')
