e58ceb4dbd3e1dfdb8799a6122c07621ad5852df
AXELWOHLIN <axelwohlin@gmail.com>
2023-03-29 16:00:22 +0200

    added plots

--- ace_test.py
+++ ace_test.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pyne.ace
 import requests
 import np_csvimport
+import numpy as np
 
 url = "https://www-nds.iaea.org/wolfram/w180/beta3/W180.ace"
 r = requests.get(url)
@@ -19,6 +20,14 @@ energy = w180.energy
 filename = 'csv_files/totalgodiva-Sheet1.csv'
 sens_vector_energy, sens_vector_values = np_csvimport.csv_import(filename)
 
+print(f"Energy xs shape is {np.shape(energy)}") #2600, 1e-11 till 5e+01
+print(f"Sens vec energy shape is {np.shape(sens_vector_energy)}") # 239, 1e-05 20000000.0
 
 
+plt.figure()
+plt.loglog(energy,total_xs)
 
+
+plt.figure(2)
+plt.loglog(sens_vector_energy,sens_vector_values)
+plt.show()
