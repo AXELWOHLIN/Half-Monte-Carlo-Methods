17b479e06f0907f322caca6e44afffa127be48a0
AXELWOHLIN <axelwohlin@gmail.com>
2023-03-29 15:01:32 +0200

    Added some ace processing tests

---
+++ ace_test.py
@@ -0,0 +1,18 @@
+import os
+import matplotlib.pyplot as plt
+import pyne.ace
+import requests
+url = "https://www-nds.iaea.org/wolfram/w180/beta3/W180.ace"
+r = requests.get(url)
+with open("W180.ace", "wb") as outfile:
+    outfile.write(r.content)
+
+lib = pyne.ace.Library('W180.ace')
+lib.read('74180.21c')
+lib.tables
+w180 = lib.tables['74180.21c']
+total_xs = w180.sigma_t
+energy = w180.energy
+
+
+
