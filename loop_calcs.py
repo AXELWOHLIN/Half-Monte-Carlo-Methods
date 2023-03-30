277edc0cc3c96ad5bcc2417605823db79b4830f9
AXELWOHLIN <axelwohlin@gmail.com>
2023-03-30 14:06:32 +0200

    Fixed centralfile comparison (pseudo)

--- loop_calcs.py
+++ loop_calcs.py
@@ -7,8 +7,23 @@ from scipy.interpolate import interp2d
 from scipy.interpolate import interp1d
 import numpy as np
 
-results_vector = np.zeros(shape = (99,1))
-file_indxs = ["00","01","02","03","04","05","06","07","08","09"] + list(range(10,100))
+central_file_name = os.path.join('U235.nuss.10.10.2016', f'U235-n.ace_0000')
+with open(central_file_name, 'rb') as infile:
+    central_file_contents = infile.read()
+
+    # Write the contents to a new file
+with open('U235central.ace', 'wb') as outfile:
+    outfile.write(central_file_contents)
+
+lib = pyne.ace.Library('U235central.ace')
+lib.read('92235.00c')
+lib.tables
+centralU235 = lib.tables['92235.00c']
+central_total_xs = centralU235.sigma_t
+
+
+results_vector = np.zeros(shape = (98,1))
+file_indxs = ["01","02","03","04","05","06","07","08","09"] + list(range(10,100))
 for i in file_indxs:
     if int(i) == 44:
         continue
@@ -34,8 +49,32 @@ for i in file_indxs:
     energy *= 1e+06
 
     sens_vec_values_adjusted = np.interp(energy,sens_vector_energy,sens_vector_values)
+    tmp = np.dot(sens_vec_values_adjusted,(total_xs.transpose()-central_total_xs.transpose()))
     if int(i)>=44:
-        results_vector[int(i)-1,0] = np.dot(sens_vec_values_adjusted,total_xs.transpose())
+        results_vector[int(i)-2,0] = tmp
     else:
-        results_vector[int(i),0] = np.dot(sens_vec_values_adjusted,total_xs.transpose())
-    print(f"Our scalar is {np.dot(sens_vec_values_adjusted,total_xs.transpose())}")
+        results_vector[int(i)-1,0] = tmp
+    print(f"Our scalar is {tmp}")
+
+mean = np.mean(results_vector)
+std_dev = np.std(results_vector)
+
+# Create a histogram of the vector with 10 bins
+plt.hist(results_vector, bins=25, density=True)
+
+# Create a range of x values for the PDF plot
+x = np.linspace(mean - 3 * std_dev, mean + 3 * std_dev, 100)
+
+# Calculate the PDF values for the x values
+pdf = 1 / (std_dev * np.sqrt(2 * np.pi)) * np.exp(-0.5 * ((x - mean) / std_dev) ** 2)
+
+# Plot the PDF as a line
+plt.plot(x, pdf)
+
+# Set the plot title and axis labels
+plt.title('Normal Distribution of Results Vector')
+plt.xlabel('Values')
+plt.ylabel('Probability Density')
+
+# Show the plot
+plt.show()
