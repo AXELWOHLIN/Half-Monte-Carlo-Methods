6a85422af53f463eabc4750830b6956e8c53f022
AXELWOHLIN <axelwohlin@gmail.com>
2023-04-05 11:30:55 +0200

    Added parallel processing for ENDF to ACE

--- ENDF_to_ACE/ENDF_ACE_openmc.py
+++ ENDF_to_ACE/ENDF_ACE_openmc.py
@@ -8,5 +8,9 @@ filename = "ENDF_to_ACE/endf_test_2/n-W180-rand-0000"
 output_directory = "processed_ace_files"
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
-openmc.data.njoy.make_ace(filename, output_dir="processed_ace_files", acer="processed_ace_files/mike",error=0.001)
+openmc.data.njoy.make_ace(filename, 
+                          output_dir="processed_ace_files",
+                          acer="myAceFile",
+                          xsdir = "myXsDir",
+                          error=0.001)
 
--- ENDF_to_ACE/PARALLELIZED.py
+++ ENDF_to_ACE/PARALLELIZED.py
@@ -3,11 +3,31 @@ import math
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import multiprocessing
 
-filename = "ENDF_to_ACE/endf_test_2/n-W180-rand-0000"
-openmc.data.njoy.make_ace(filename)
+def process_file(index, input_filename, input_directory, output_directory):
+    input_filepath = os.path.join(input_directory, input_filename)
+    base_filename = "mp_ace"#os.path.splitext(input_filename)[0]
+    output_filename = f"{index}_{base_filename}.ace"
 
-openmc.data.njoy.make_ace(filename, acer="processed_ace_files/mike", xsdir="processed_ace_files/mikexs",\
-                        pendf=False, error=0.001, broadr=False, \
-                        heatr=False, gaspr=False, purr=False, evaluation=None, \
-                        smoothing=True)
+    openmc.data.njoy.make_ace(input_filepath,
+                              output_dir=output_directory,
+                              acer=output_filename, #os.path.join(output_directory, output_filename),
+                              xsdir=f"{index}_xsdir", #os.path.join(output_directory, f"{index}_XsDir"),
+                              error=0.001)
+
+input_directory = "ENDF_to_ACE/endf_test_2"
+output_directory = "parallel_ace_files"
+
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+pool = multiprocessing.Pool()
+
+for i, filename in enumerate(os.listdir(input_directory)):
+    #if filename.endswith(".endf"):
+    pool.apply_async(process_file, args=(i, filename, input_directory, output_directory))
+
+pool.close()
+pool.join()
+
