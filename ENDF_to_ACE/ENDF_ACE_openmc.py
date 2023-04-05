ee881e02b6b974310fbb434ec25ecf5ed5a6bbae
AXELWOHLIN <axelwohlin@gmail.com>
2023-04-05 19:44:19 +0200

    Did 9 u235 ace files from endf

--- ENDF_to_ACE/ENDF_ACE_openmc.py
+++ ENDF_to_ACE/ENDF_ACE_openmc.py
@@ -8,9 +8,10 @@ filename = "ENDF_to_ACE/endf_test_2/n-W180-rand-0000"
 output_directory = "ENDF_to_ACE/processed_ace_files"
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
-openmc.data.njoy.make_ace(filename, 
-                          output_dir=output_directory,
-                          acer="ENDF_to_ACE/processed_ace_files/myAceFile",
-                          xsdir = "ENDF_to_ACE/processed_ace_files/myXsDir",
-                          error=0.001)
 
+input_directory = "ENDF_to_ACE/u235_ENDF_test"
+for i, filename in enumerate(os.listdir(input_directory)):
+    openmc.data.njoy.make_ace(input_directory+"/"+filename, 
+                            output_dir=output_directory,
+                            acer="ENDF_to_ACE/processed_ace_files/{i}_AceFile",
+                            error=0.001)
--- ENDF_to_ACE/try_parall.py
+++ ENDF_to_ACE/try_parall.py
@@ -1,25 +1,20 @@
 import openmc
-import math
 import os
-import numpy as np
-import matplotlib.pyplot as plt
 import multiprocessing
 
 def process_file(index, input_filename, input_directory, output_directory):
     input_filepath = os.path.join(input_directory, input_filename)
-    base_filename = "mp_ace" #os.path.splitext(input_filename)[0]
-    output_filename = f"{index}_{base_filename}.ace"
+    output_filename = f"{index}_mp_ace.ace"
 
     with open(os.path.join(output_directory, f"{index}_XsDir"), "w") as xsdir_file:
         openmc.data.njoy.make_ace(input_filepath,
                                   output_dir=output_directory,
-                                  acer=True,
                                   xsdir=xsdir_file,
                                   error=0.001)
 
     # Rename the generated ACE file
     os.rename(os.path.join(output_directory, "ace_293.6"),
-              os.path.join(output_directory, f"{index}_myAceFile"))
+              os.path.join(output_directory, output_filename))
 
 input_directory = "ENDF_to_ACE/endf_test_2"
 output_directory = "ENDF_to_ACE/parallel_ace_files"
@@ -30,7 +25,6 @@ if not os.path.exists(output_directory):
 pool = multiprocessing.Pool()
 
 for i, filename in enumerate(os.listdir(input_directory)):
-    #if filename.endswith(".endf"):
     pool.apply_async(process_file, args=(i, filename, input_directory, output_directory))
 
 pool.close()
