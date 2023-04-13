4765ca2b17de76cfac70f8f44ee5d991b5b0a999
AXELWOHLIN <axelwohlin@gmail.com>
2023-04-13 16:00:10 +0200

    La till W180 NJOY ACE och static interp

--- ENDF_to_ACE/ENDF_ACE_openmc.py
+++ ENDF_to_ACE/ENDF_ACE_openmc.py
@@ -5,13 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 filename = "ENDF_to_ACE/endf_test_2/n-W180-rand-0000"
-output_directory = "ENDF_to_ACE/processed_ace_files"
+output_directory = "ENDF_to_ACE/olle_mike_hawk"
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
-input_directory = "ENDF_to_ACE/u235_ENDF_test"
+input_directory = "ENDF_to_ACE/endf_test_2"
 for i, filename in enumerate(os.listdir(input_directory)):
     openmc.data.njoy.make_ace(input_directory+"/"+filename, 
                             output_dir=output_directory,
-                            acer="ENDF_to_ACE/processed_ace_files/{i}_AceFile",
-                            error=0.001)
+                            acer="ENDF_to_ACE/olle_mike_hawk/{i}_AceFile",
+                            error=0.00001)
