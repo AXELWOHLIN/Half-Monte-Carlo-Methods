6c455e52724f94a5f9d0d6f73be5e4b44d4cf37c
olledios <olle.dios1@gmail.com>
2023-04-14 11:29:50 +0200

    cmtmsg

--- missingenergyindex.py
+++ missingenergyindex.py
@@ -15,7 +15,7 @@ from tkinter.filedialog import askopenfilename
 
 edict = {}
 k = 0
-for file in os.scandir('U235.nuss.10.10.2016'):
+for file in os.scandir('ENDF_to_ACE/olle_mike_hawk'):
     filename = os.fsdecode(file)
     if ".ace" in filename:
         with open(filename, 'rb') as infile:
@@ -25,16 +25,17 @@ for file in os.scandir('U235.nuss.10.10.2016'):
         with open('ace_file.ace', 'wb') as outfile:
             outfile.write(ace_file_contents)
         lib = pyne.ace.Library('ace_file.ace')
-        lib.read('92235.00c')
+        lib.read('74180.01c')
         lib.tables
-        file_contents = lib.tables['92235.00c']
-        edict[k] = file_contents.energy
+        file_contents = lib.tables['74180.01c']
+        edict[k] = [file_contents.energy, file_contents.sigma_t]
         k += 1
 
-e0 = edict[5]
-e1 = edict[3]
-e2 = edict[2]
+e0, t0 = edict[0]
+e1, t1 = edict[1]
 
+
+""""
 b = 0
 for i in range(len(e0)):
     if e0[i]==e1[i]:
@@ -49,16 +50,16 @@ for i in range(len(e0)):
         print(e1[i])
         print()
         b += 1
-
+"""
 
 diff = len(e1) - len(e0)
-
+"""
 # Append zeros to the shorter array
 if diff > 0:
     e0 = np.append(e0, np.zeros(diff))
 else:
     e1 = np.append(e1, np.zeros(abs(diff)))
+"""
 
-
-plt.loglog(e0, e1)
+plt.loglog(e1, t1)
 plt.show()
