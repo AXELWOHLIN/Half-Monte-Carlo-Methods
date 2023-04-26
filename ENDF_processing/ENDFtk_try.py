cb28d2ca6155091c01eddd3accaa1d27e3305e95
AXELWOHLIN <axelwohlin@gmail.com>
2023-04-26 14:34:15 +0200

    Failed ENDF attempt #492193219301204i320949 by Axl

--- ENDF_processing/ENDFtk_try.py
+++ ENDF_processing/ENDFtk_try.py
@@ -5,6 +5,19 @@ sys.path.append('/home/axelwohlin/Desktop/ENDFtk/build')
 import ENDFtk
 #Specify the path back to your clone of the git directory
 sys.path.append('/home/axelwohlin/Desktop/kand')
-filename = "ENDF_processing/N_ELMT2.txt"
+filename = 'ENDF_processing/N_ELMT2.txt'#'ENDF_processing/1_2_4_16_17_18_102.txt'
+#'ENDF_processing/E4R18790_e4.txt'
+#'ENDF_processing/N_ELMT2.txt'#
+
+# Load ENDF-6 file
 tape = ENDFtk.tree.Tape.from_file(filename)
-print(tape.MAT(9228))
+
+# Update MAT code to 9228
+mat = tape.material(9228).parse() # Update MAT code to 9228
+
+# Access file and section for desired data
+file = mat.file(33).parse()
+section = mat.file(33).section(3).parse()
+
+# Now you can work with the data in 'file' and 'section' objects
+# for the desired material (Uranium-235 with MAT code 9228)
