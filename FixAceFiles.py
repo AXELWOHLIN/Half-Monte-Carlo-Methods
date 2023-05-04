677aec7f6a7988e8c8e473addcf16ae12946a68d
vili1372 <viktorrlindstromm@gmail.com>
2023-05-04 22:55:05 +0200

    Fixed ace files and ran openmc

--- FixAceFiles.py
+++ FixAceFiles.py
@@ -1,5 +1,6 @@
 import os
 
+ace_files=[]
 dir_ace='/home/rfp/kand/u235.nuss.30.04.2023/'
 
 os.chdir(dir_ace)
@@ -7,9 +8,10 @@ for entry in os.scandir('.'):
     if entry.is_file() and ".ace" in entry.name:
         ace_files.append(str(entry.name))
             
-            
-    ace_files=sorted(ace_files)
-    ace_files=ace_files[1:]
+
+ace_files=sorted(ace_files)
+ace_files=ace_files[1:]
+    
 
 
 for ace_file in ace_files:
@@ -17,7 +19,8 @@ for ace_file in ace_files:
     content = file.read()
     lines = content.splitlines()
     lines[-1]=lines[-1][:20]
-    
+    print(lines[-1])
     file.seek(0)
-
-    file.write
+    file.writelines('\n'.join(lines))
+    file.truncate()
+    file.close()
